e3b84eecdc6e4da8ab4298ca959fa0693a54b929
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-05 18:31:27 -0300

    Update File1.java

--- File1.java
+++ File1.java
@@ -1,11 +1,3 @@
-/*
-Atividade 1
-
-Vamos praticar!!
-▪ Implemente uma função que recebe um path para um arquivo e
-▪ Se ele existe, então lê o conteúdo do arquivo e mostra na tela
-▪ Se ele não existe, então cria um arquivo e escreve o nome do aluno (seu nome)
- */
 package file1;
 
 import java.io.*;
